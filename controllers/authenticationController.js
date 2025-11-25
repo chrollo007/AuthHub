00d10cce6cc0f99815e7c448fd3c8d6ac6334d22
chrollo007 <amanbharadwaj75@gmail.com>
2025-11-26 00:21:11 +0530

    fix minor issue

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -7,22 +7,22 @@ async function hello(req,res){
 async function register(req,res){
    const {name,email,password}=req.body;
    if(object.find(obj=>obj.email===email)){
-    res.json("email already exist");
+    return res.json("email already exist");
    }
     object.push({name,email,password});
-   res.json("registered successfully")
+   return res.json("registered successfully")
     
 }
 async function get(req,res){
-   res.json(object)
+   return res.json(object)
 }
 async function login(req,res){
     const {email,password}=req.body;
     const exist=object.find(obj=>obj.email===email)
     if(!exist){
-            res.json("no user found")
-    }else if(object.password===password){
-        res.json("login successfully");
+            return res.json("no user found")
+    }else if(exist.password===password){
+        return res.json("login successfully");
     }
     
 }
